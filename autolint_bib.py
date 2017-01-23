09a4bbdaab524e0701c0cd0c8c9949bce43a2477
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-01-23 22:39:07 +0900

    Add citations I used in advanced robotics 2016

--- autolint_bib.py
+++ autolint_bib.py
@@ -13,17 +13,26 @@ def main():
     with open(bib_file) as f:
         bib = bibtexparser.loads(f.read())
 
-    required_fields = ['title', 'author', 'booktitle', 'link']
+    required_fields = ['title', 'author', ['booktitle', 'journal'], 'link']
     for entry in bib.entries:
         cite_id = entry['ID']
-        missing = [k for k in required_fields if k not in entry]
+        missing = []
+        for k in required_fields:
+            if isinstance(k, list):
+                if not any(kk in entry for kk in k):
+                    missing.append(' or '.join(k))
+            else:
+                if not k in entry:
+                    missing.append(k)
         if missing:
-            print('WARNING: entry [%s] has missing info: %s' %
+            print('ERROR: entry [%s] has missing info: %s' %
                   (cite_id, ', '.join(missing)))
+
+        entry['author'] = entry['author'].replace('\n', ' ')
 
     writer = bibtexparser.bwriter.BibTexWriter()
     with open(bib_file, 'w') as f:
-        f.write(writer.write(bib)[:-1])
+        f.write(writer.write(bib).encode('utf-8')[:-1])
 
 
 if __name__ == '__main__':
--- bib_to_readme.py
+++ bib_to_readme.py
@@ -9,6 +9,36 @@ import tabulate
 def abbreviate_booktitle(booktitle, year):
     if 'Computer Vision and Pattern Recognition' in booktitle:
         return 'CVPR' + year
+    elif 'International Conference on Robotics and Automation' in booktitle:
+        return 'ICRA' + year
+    elif 'International Conference on Humanoid Robots' in booktitle:
+        return 'Humanoids' + year
+    elif 'Intelligent Robots and Systems' in booktitle:
+        return 'IROS' + year
+    elif 'International Conference on Machine Learning' in booktitle:
+        return 'ICML' + year
+    elif 'International Conference on Learning Representations' in booktitle:
+        return 'ICLR' + year
+    elif 'International Symposium on Experimental Robotics' in booktitle:
+        return 'ISER' + year
+    elif 'ISRR' in booktitle:
+        return 'ISRR' + year
+    elif 'Proceedings of Robotics: Science and Systems' in booktitle:
+        return 'RSS' + year
+    elif 'International Conference on Technologies for Practical Robot Applications' in booktitle:
+        return 'TePRA' + year
+    elif 'International Conference on Mechatronics and Automation' in booktitle:
+        return 'ICMA' + year
+    elif 'International Journal of Computer Vision' in booktitle:
+        return 'IJCV' + year
+    elif 'International Journal of Robotics Research' in booktitle:
+        return 'IJRR' + year
+    elif 'Neural Information Processing Systems' in booktitle:
+        return 'NIPS' + year
+    elif 'DAAAM' in booktitle:
+        return 'DAAAM' + year
+    elif 'arXiv' in booktitle:
+        return 'arXiv' + year
     else:
         return booktitle
 
@@ -41,7 +71,8 @@ def main():
             tex = '`\cite{%s}`' % entry['ID']
             linked_title = '[%s](%s)' % (entry['title'], entry['link'])
             author = abbreviate_author(entry['author'])
-            book = abbreviate_booktitle(entry['booktitle'], entry['year'])
+            booktitle = entry.get('booktitle') or entry['journal']
+            book = abbreviate_booktitle(booktitle, entry['year'])
             citations.append((tex, linked_title, author, book))
         md_table = tabulate.tabulate(citations, headers=headers,
                                     tablefmt='pipe', stralign='center')
